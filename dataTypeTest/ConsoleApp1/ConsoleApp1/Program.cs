26e0059cf630892df643cb6206526eaf2bf67e5c
kyle-luo <tsjlky@gmail.com>
2020-08-06 00:56:58 -0400

    Update Program.cs

--- dataTypeTest/ConsoleApp1/ConsoleApp1/Program.cs
+++ dataTypeTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,30 @@ namespace ConsoleApp1
             bool[,,] cells;
             cells = new bool[2, 3, 3];
             Console.WriteLine(cells.GetLength(1));
+
+            string reverse, palindrome;
+            char[] temp;
+            System.Console.Write("Enter a palindrome: ");
+            palindrome = System.Console.ReadLine();
+            // Remove spaces and convert to lowercase
+            reverse = palindrome.Replace(" ", "");
+            reverse = reverse.ToLower();
+            // Convert to an array
+            temp = reverse.ToCharArray();
+            // Reverse the array
+            System.Array.Reverse(temp);
+            // Convert the array back to a string and
+            // check if reverse string is the same.
+            if (reverse == new string(temp))
+            {
+                System.Console.WriteLine(
+                $"\"{palindrome}\" is a palindrome.");
+            }
+            else
+            {
+                System.Console.WriteLine(
+                $"\"{palindrome}\" is NOT a palindrome.");
+            }
         }
     }
 }
