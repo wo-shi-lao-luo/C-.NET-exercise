d210632103b4b43d4b6c5149973c804638e07c49
kyle-luo <tsjlky@gmail.com>
2020-08-06 00:53:38 -0400

    datatype

---
+++ dataTypeTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool[,,] cells;
+            cells = new bool[2, 3, 3];
+            Console.WriteLine(cells.GetLength(1));
+        }
+    }
+}
