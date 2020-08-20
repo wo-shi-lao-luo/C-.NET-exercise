c1926977bef8b6cabd7423f4c196efc0d8046a61
kyle-luo <tsjlky@gmail.com>
2020-08-19 22:07:09 -0400

    Update Program.cs

--- dataTypeTest/ConsoleApp1/ConsoleApp1/Program.cs
+++ dataTypeTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,34 +14,39 @@ namespace ConsoleApp1
             cells = new bool[2, 3, 3];
             Console.WriteLine(cells.GetLength(1));
 
-            string reverse, palindrome;
-            char[] temp;
-            System.Console.Write("Enter a palindrome: ");
-            palindrome = System.Console.ReadLine();
-            // Remove spaces and convert to lowercase
-            reverse = palindrome.Replace(" ", "");
-            reverse = reverse.ToLower();
-            // Convert to an array
-            temp = reverse.ToCharArray();
-            // Reverse the array
-            System.Array.Reverse(temp);
-            // Convert the array back to a string and
-            // check if reverse string is the same.
-            if (reverse == new string(temp))
-            {
-                System.Console.WriteLine(
-                $"\"{palindrome}\" is a palindrome.");
-            }
-            else
-            {
-                System.Console.WriteLine(
-                $"\"{palindrome}\" is NOT a palindrome.");
-            }
+            //string reverse, palindrome;
+            //char[] temp;
+            //System.Console.Write("Enter a palindrome: ");
+            //palindrome = System.Console.ReadLine();
+            //// Remove spaces and convert to lowercase
+            //reverse = palindrome.Replace(" ", "");
+            //reverse = reverse.ToLower();
+            //// Convert to an array
+            //temp = reverse.ToCharArray();
+            //// Reverse the array
+            //System.Array.Reverse(temp);
+            //// Convert the array back to a string and
+            //// check if reverse string is the same.
+            //if (reverse == new string(temp))
+            //{
+            //    System.Console.WriteLine(
+            //    $"\"{palindrome}\" is a palindrome.");
+            //}
+            //else
+            //{
+            //    System.Console.WriteLine(
+            //    $"\"{palindrome}\" is NOT a palindrome.");
+            //}
+
+            string testNull = null;
+            Console.WriteLine(testNull?.ToString());
+            string testNotNull = "ABCD";
+            Console.WriteLine(testNotNull?.ToLower());
         }
 
         public Boolean Test(Boolean input)
         {
-
+            return true;
         }
     }
 }
