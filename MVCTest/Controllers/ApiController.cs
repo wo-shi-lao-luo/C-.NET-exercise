eb5a3c6e3bba3602ab1da19debacb8370f4fa490
kyle-luo <tsjlky@gmail.com>
2020-10-17 00:14:25 -0400

    update

---
+++ MVCTest/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class ApiController : Controller
+    {
+        [HttpGet("people/all")]
+        public ActionResult<IEnumerable<Person>> GetAll()
+        {
+            return new[]
+            {
+            new Person { Name = "Ana" },
+            new Person { Name = "Felipe" },
+            new Person { Name = "Emillia" }
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
